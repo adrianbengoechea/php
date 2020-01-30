cf8564ee3545252d77588288a3191a400922ec03
shocky <ebel.bengoechea@gmail.com>
2020-01-30 16:30:23 -0600

    FIXES

--- arrays.php
+++ arrays.php
@@ -2,13 +2,13 @@
 $days = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
 $array = array('text', 12, 12.55, true, array(1, 2, 3));
 
-echo $week[0] . '<br>';
-echo $week[1] . '<br>';
-echo $week[2] . '<br>';
-echo $week[3] . '<br>';
-echo $week[4] . '<br>';
-echo $week[5] . '<br>';
-echo $week[6];
+echo $days[0] . '<br>';
+echo $days[1] . '<br>';
+echo $days[2] . '<br>';
+echo $days[3] . '<br>';
+echo $days[4] . '<br>';
+echo $days[5] . '<br>';
+echo $days[6];
 
 
  ?>
