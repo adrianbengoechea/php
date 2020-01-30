0d23f69edc6b71f0838d948b1f8afc7b16629ed4
shocky <ebel.bengoechea@gmail.com>
2020-01-30 12:42:24 -0600

    FIRST COMMIT

---
+++ constants.php
@@ -0,0 +1,11 @@
+<?php
+
+  define('PI', 3.1415);
+  define('NAME', 'CESARXD');
+
+  echo NAME;
+
+  // $name = 'CESARXD';
+  // echo $name
+
+?>
---
+++ variables.php
@@ -0,0 +1,27 @@
+<?php
+
+  $name = 'senna<br>';
+  $number = 7;
+
+  echo $name;
+
+  # STRING
+  $string = 'Text, text and more text';
+
+  # INTEGER
+  $integer = 8;
+
+  # DOUBLE
+  $double =   69.69;
+
+  # BOOLEAN
+  $boolean = true;
+
+  # Array
+  # Object
+  # Class
+  # Null
+
+  echo gettype($integer);
+
+ ?>
