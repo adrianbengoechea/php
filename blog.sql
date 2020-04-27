a569944dd98f3008a9978fb10388e853036f30be
ENKI <enki.ndm@gmail.com>
2020-04-27 01:39:58 -0600

    DB Update - beta 1.0.1

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-04-2020 a las 08:01:38
+-- Tiempo de generación: 27-04-2020 a las 09:39:20
 -- Versión del servidor: 10.4.8-MariaDB
 -- Versión de PHP: 7.3.11
 
@@ -31,7 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `data` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `extract` varchar(255) NOT NULL,
+  `extract` varchar(255) DEFAULT NULL,
   `date` varchar(55) NOT NULL DEFAULT '',
   `text` text NOT NULL,
   `thumbnail` varchar(55) NOT NULL,
@@ -61,7 +61,8 @@ INSERT INTO `data` (`id`, `title`, `extract`, `date`, `text`, `thumbnail`, `auth
 (59, 'Article # 16', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan est nec ligula condimentum elementum. Aenean dictum arcu vel mollis rutrum. Aliquam libero augue, egestas nec nisl ut, facilisis auctor erat. # 16', '04/17/2020', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan est nec ligula condimentum elementum. Aenean dictum arcu vel mollis rutrum. Aliquam libero augue, egestas nec nisl ut, facilisis auctor erat. Sed risus dolor, vulputate porta sapien nec, facilisis finibus ante. Vivamus aliquet massa sit amet leo imperdiet hendrerit. Nulla ultricies ligula sed lobortis venenatis. Donec in tempor tellus. Mauris quis mattis leo, at eleifend odio. Morbi pellentesque lacus eget tortor dictum commodo. Fusce a tortor eu tellus ultricies viverra at at ante. Curabitur vitae dolor ut lacus volutpat viverra sit amet sed libero. Cras efficitur metus bibendum, faucibus est non, rutrum purus. Praesent sit amet mi a ex porttitor rhoncus. Cras ut quam volutpat, porttitor arcu ut, aliquam eros. Quisque eleifend efficitur ligula feugiat ultricies. # 16', 'photo-16.jpg', 'admin'),
 (60, 'Article # 17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan est nec ligula condimentum elementum. Aenean dictum arcu vel mollis rutrum. Aliquam libero augue, egestas nec nisl ut, facilisis auctor erat. # 17', '04/17/2020', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan est nec ligula condimentum elementum. Aenean dictum arcu vel mollis rutrum. Aliquam libero augue, egestas nec nisl ut, facilisis auctor erat. Sed risus dolor, vulputate porta sapien nec, facilisis finibus ante. Vivamus aliquet massa sit amet leo imperdiet hendrerit. Nulla ultricies ligula sed lobortis venenatis. Donec in tempor tellus. Mauris quis mattis leo, at eleifend odio. Morbi pellentesque lacus eget tortor dictum commodo. Fusce a tortor eu tellus ultricies viverra at at ante. Curabitur vitae dolor ut lacus volutpat viverra sit amet sed libero. Cras efficitur metus bibendum, faucibus est non, rutrum purus. Praesent sit amet mi a ex porttitor rhoncus. Cras ut quam volutpat, porttitor arcu ut, aliquam eros. Quisque eleifend efficitur ligula feugiat ultricies. # 17', 'photo-17.jpg', 'admin'),
 (61, 'Article # 18', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan est nec ligula condimentum elementum. Aenean dictum arcu vel mollis rutrum. Aliquam libero augue, egestas nec nisl ut, facilisis auctor erat. # 18', '04/17/2020', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan est nec ligula condimentum elementum. Aenean dictum arcu vel mollis rutrum. Aliquam libero augue, egestas nec nisl ut, facilisis auctor erat. Sed risus dolor, vulputate porta sapien nec, facilisis finibus ante. Vivamus aliquet massa sit amet leo imperdiet hendrerit. Nulla ultricies ligula sed lobortis venenatis. Donec in tempor tellus. Mauris quis mattis leo, at eleifend odio. Morbi pellentesque lacus eget tortor dictum commodo. Fusce a tortor eu tellus ultricies viverra at at ante. Curabitur vitae dolor ut lacus volutpat viverra sit amet sed libero. Cras efficitur metus bibendum, faucibus est non, rutrum purus. Praesent sit amet mi a ex porttitor rhoncus. Cras ut quam volutpat, porttitor arcu ut, aliquam eros. Quisque eleifend efficitur ligula feugiat ultricies. # 18', 'photo-18.jpg', 'admin'),
-(62, 'asdasd', 'asdasd', '04/27/2020', 'asdasd', 'Mimi 20190525_122037.jpg', 'admin');
+(63, 'The Test', 'Test Excerpt', '04/27/2020', 'Test Text and Content \r\n\r\nCONTENT CONTENT', 'Screenshot_3.png', 'admin'),
+(64, 'Update Article Test 10', '', '04/27/2020', 'Update Article Test Content', 'Screenshot_2.png', 'admin');
 
 -- --------------------------------------------------------
 
@@ -136,7 +137,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `data`
 --
 ALTER TABLE `data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT de la tabla `settings`
