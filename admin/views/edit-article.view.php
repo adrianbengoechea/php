33ec42290a49789d1c39a9fa8399c3d65506409c
ENKI <enki.ndm@gmail.com>
2020-04-27 01:38:40 -0600

    The Master Blog S - beta 1.0.0

--- admin/views/edit-article.view.php
+++ admin/views/edit-article.view.php
@@ -1,34 +1,73 @@
 <?php require_once 'parts/header.php'; 
 
+   $title_content = '';
+   $excerpt_content = '';
+   $text_content = ''; 
+   $thumb_content = '';
 
+   if(isset($_GET) && !empty($_GET) && isset($_GET['id']) && !empty(trim($_GET['id'])) ){
+       
+        $delete_id = '?id=' . $_GET['id'];
+        $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+        $data = db("SELECT * FROM data WHERE id = :q1", array(':q1'=>$id));
 
-   if(isset($_POST) && !empty($_POST)){
-    $title = filter_var($_POST['article_title'], FILTER_SANITIZE_STRING);
-    $extract = filter_var($_POST['article_extract'], FILTER_SANITIZE_STRING);
-    $text = filter_var($_POST['article_content'], FILTER_SANITIZE_STRING);
+        if(!empty($data)){
 
-    
-    $fl = $_FILES['article_thumbnail'];
-    $thumbnail = $fl['tmp_name'];
-    $thumbnail_url = '../assets/img/' . $fl['name'];
-    
-    if(!empty(trim($title)) && !empty(trim($text)) ){
-        if(!empty($fl)){
-            if($fl['type'] == 'image/png' || $fl['type'] == 'image/jpeg'){
-                move_uploaded_file($thumbnail, $thumbnail_url);
-            }
-        }
-        if(isset($_FILES) && !empty($_FILES)){
-            db('INSERT INTO `data` (`title`, `extract`, `date`, `text`, `thumbnail`, `author`) VALUES (:q1, :q2, :q3, :q4, :q5, :q6)', array(':q1'=>$title, ':q2'=>$extract, ':q3'=>$date, ':q4'=>$text, ':q5'=>$fl['name'], ':q6'=>$author));
+            $title_content .= $data[0]['title'];
+            $excerpt_content .= $data[0]['extract'];
+            $text_content .= $data[0]['text'];
+            $thumb_content .= $data[0]['thumbnail'];
+
         }else{
-            db('INSERT INTO `data` (`title`, `extract`, `date`, `text`, `thumbnail`, `author`) VALUES (:q1, :q2, :q3, :q4, :q5, :q6)', array(':q1'=>$title, ':q2'=>$extract, ':q3'=>$date, ':q4'=>$text, ':q5'=>$fl['name'], ':q6'=>$author));
+            return_admin();
         }
-
+    }else{
+        return_admin();
     }
-    
 
-    
-   } 
+    if(isset($_POST) && !empty($_POST)){
+        $title = filter_var($_POST['article_title'], FILTER_SANITIZE_STRING);
+        $extract = filter_var($_POST['article_extract'], FILTER_SANITIZE_STRING);
+        $text = filter_var($_POST['article_content'], FILTER_SANITIZE_STRING);
+        $thum = filter_var($_POST['article_actual_thum'], FILTER_SANITIZE_URL);
+        $post_id = filter_var($_POST['article_id'], FILTER_SANITIZE_NUMBER_INT);
+
+        
+        if(isset($_FILES) && !empty($_FILES)){
+            $fl = $_FILES['article_thumbnail'];
+            $thumbnail = $fl['tmp_name'];
+            $thumbnail_url = '../assets/img/' . $fl['name'];
+        }
+        
+        if(!empty(trim($title)) && !empty(trim($text)) && !empty(trim($post_id)) ){
+            
+            
+
+            if(isset($fl['name']) && !empty($fl['name'])){
+                $thum = $fl['name'];
+            }
+
+            db("UPDATE `data` SET `title` = :q1, `extract` = :q2, `text` = :q3, `thumbnail` = :q4 WHERE `id` = :q5", array(':q1'=>$title, ':q2'=>$extract, ':q3'=>$text, ':q4'=>$thum, ':q5'=>$post_id));
+            refresh();
+                
+
+        }else{
+            refresh();
+        }
+    } 
+   
+
+  
+
+
+
+
+
+
+
+
+
+   
    ?>
 
     <main class="wrapper">
@@ -40,38 +79,9 @@
         </aside>
 
         <section class="wrapper-content container-fluid">
-            <h2 class="mb-4">ADD NEW ARTICLE</h2>
+            <h2 class="mb-4">EDIT ARTICLE</h2>
 
-            <?php 
-
-                $title_content = '';
-                $excerpt_content = '';
-                $text_content = ''; 
-                $thumb_content = '';
-
-                if(isset($_GET) && !empty($_GET) && isset($_GET['id']) && !empty(trim($_GET['id'])) ){
-                    
-                        $delete_id = '?id=' . $_GET['id'];
-                        $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
-                        $data = db("SELECT * FROM data WHERE id = :q1", array(':q1'=>$id));
-
-                        if(!empty($data)){
-
-                            $title_content .= $data[0]['title'];
-                            $excerpt_content .= $data[0]['extract'];
-                            $text_content .= $data[0]['text'];
-                            $thumb_content = $data[0]['thumbnail'];
-
-                        }else{
-                            return_admin();
-                        }
-                }else{
-                    return_admin();
-                }
-
-                ?>
-
-                <form enctype="multipart/form-data" id="edit-article-form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+                <form enctype="multipart/form-data" id="edit-article-form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) . $delete_id ?>" method="POST">
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-12">
@@ -97,7 +107,12 @@
                                     <h6>Thumbnail: </h6>
                                     <input type="file" name="article_thumbnail" id="article_thumbnail">
                                 </label>
-                                
+                            </div>
+                            <div class="col-12">
+                                <input type="hidden" name="article_id" value="<?php echo $id; ?>">
+                            </div>
+                            <div class="col-12">
+                                <input type="hidden" name="article_actual_thum" value="<?php echo $thumb_content; ?>">
                             </div>
 
                             <div class="col-12">
